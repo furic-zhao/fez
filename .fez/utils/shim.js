b2e4e85b2c9a4ad89abc690696a1c782719a31c8
zhaohuijian <zhaohuijian@jd.com>
2017-03-14 09:23:55 +0800

    .fezrc中添加shim配置

---
+++ .fez/utils/shim.js
@@ -0,0 +1,14 @@
+let config = require('rc')('fez', {
+    "browserify": {
+        "shim": []
+    }
+});
+
+let shim = {};
+
+for (let v of config.browserify.shim) {
+    shim[v.from] = {
+        exports: `global:${v.import}`
+    };
+}
+module.exports = shim;
