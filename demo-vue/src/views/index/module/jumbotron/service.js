4b12d4d2dfec639d4a29aff75c0c6f2eb05eff09
zhaohuijian <zhaohuijian@jd.com>
2017-03-25 10:48:16 +0800

    优化vue示例

--- demo-vue/src/views/index/module/jumbotron/service.js
+++ demo-vue/src/views/index/module/jumbotron/service.js
@@ -1,6 +1,6 @@
 let rtnData = {
-    title: "Hello, FEZ!",
-    content: "这是一个使用 FEZ 构建的系统模板，演示了基于Vue单文件组件的开发结构。",
+    title: "Hello, FEZ And Vue!",
+    content: "这是一个使用 FEZ 构建的前端模块化开发示例，演示了基于Vue单文件组件的开发结构。",
     button: {
         name: `<i class="fa fa-github-alt"></i> GitHub`,
         href: "https://github.com/furic-zhao/fez"
--- demo-vue/src/views/list/module/jumbotron/service.js
+++ demo-vue/src/views/list/module/jumbotron/service.js
@@ -1,6 +1,6 @@
 let rtnData = {
-    title: "FEZ Frame!",
-    content: "这是一个FEZ整合Vue的列表展示页，请点击下面列表的 Detail 进入详情页浏览",
+    title: "FEZ And Vue Frame!",
+    content: "这是一个FEZ整合Vue的列表展示页，请点击下面列表的 Detail 按钮进入详情页浏览",
     button: {
         name: "查看更多信息",
         href: "http://fez.hestudy.com"
