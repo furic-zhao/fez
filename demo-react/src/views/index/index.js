cbe33aff7c43c0d60cfa844247cc0b3c55d58ab3
zhaohuijian <zhaohuijian@jd.com>
2017-03-30 09:19:13 +0800

    优化demo示例

--- demo-react/src/views/index/index.js
+++ demo-react/src/views/index/index.js
@@ -7,7 +7,7 @@ import Fezdesc from './module/fezdesc/index';
 import Footer from 'public/module/common/footer';
 
 
-var Page = React.createClass({
+let Page = React.createClass({
 	render() {
 		return (
 			<div>
