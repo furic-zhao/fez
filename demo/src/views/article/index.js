4c7eac371ca1dcec670ea0011b53080b847e7ff0
liuhui162 <liuhui15@jd.com>
2017-04-05 09:49:02 +0800

    11

--- demo/src/views/article/index.js
+++ demo/src/views/article/index.js
@@ -7,6 +7,7 @@ footer($("#js-footer"));
 // import jumbotron from './module/jumbotron';
 // jumbotron($("#js-jumbotron"));
 
+console.log(00000000);
 import detail from './module/detail';
 detail($("#js-detail"));
 
